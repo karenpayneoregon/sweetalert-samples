7f6285fe1cc87aa4686d9b27e7856af22ea49f0f
Karen Payne <paynekaren@comcast.net>
2023-05-08 10:39:15 -0700

    icon left

---
+++ SweetAlertExamples/Pages/IconPositionPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SweetAlertExamples.Pages
+{
+    public class IconPositionPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
