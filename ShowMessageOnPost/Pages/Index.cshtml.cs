4a439c17eb10ea6d2a85a1b3bc69fcb1db930582
Karen Payne <paynekaren@comcast.net>
2023-05-11 06:16:50 -0700

    new project

---
+++ ShowMessageOnPost/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+#pragma warning disable CS8618
+
+namespace ShowMessageOnPost.Pages;
+public class IndexModel : PageModel
+{
+    public string SuccessMessage { get; set; }
+
+    public void OnGet(string sender)
+    {
+        if (sender is { })
+        {
+            SuccessMessage = "Finished";
+        }
+    }
+    public Task<IActionResult> OnPostSubmit() =>
+        Task.FromResult<IActionResult>(RedirectToPage("Index", new
+        {
+            sender = "Whatever"
+        }));
+
+    public PageResult OnPostSubmit1() => Page();
+}
---
+++ ShowMessageOnPost/readme.md
@@ -0,0 +1,5 @@
+﻿# About
+
+Basic on presenting a popup from document load
+
+![Figure1](assets/figure1.png)
